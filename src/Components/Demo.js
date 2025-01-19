58fc0f03b516ceecb0cce3d426896c770f64deb2
Shrilakshmi Kulkarni <62303301+kulkarnishrilakshmi@users.noreply.github.com>
2025-01-19 11:12:44 +0530

    Class Component

---
+++ src/Components/Demo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Demo extends React.Component{
+    render(){                         //render - Returns html code
+        return <h2>Hi i am Shrilakshmi Kulkarni - Demo Component</h2>;
+    }
+}
+
+export default Demo;
